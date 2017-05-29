fdaf74cb0072b9a5afb97520a89a93b8c96422cd
liangjia <liangjia@qq.com>
2017-05-29 20:44:33 +0800

    创建Odoo 8.0模块Partner Multi Address客户多地址模块

---
+++ __openerp__.py
@@ -0,0 +1,22 @@
+﻿# -*- coding: utf-8 -*-
+{
+    'name': 'Partner-Multi-Address',
+    'version': '0.1',
+    'category': 'Tools',
+    'description': """
+This module is for managing a partner multiaddress environment.
+=======================================================
+
+This module is the ext module for other multi-address modules.
+    """,
+    'author': 'LiangJia@qq.com',
+    'website': 'http://www.duuge.com',
+    'depends': [
+        'base',
+        'sale_stock',
+    ],
+    'data': ['res_address_view.xml'],
+    'demo': ['multi_address_demo.xml'],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ partner_multi_address.py
@@ -0,0 +1,11 @@
+﻿#-*- coding:utf-8 -*-
+from openerp.osv import fields,osv
+
+class partner_multi_address(osv.osv):
+    _name="res.partner"
+    _inherit="res.partner"
+    _description="add Partner Multi Address."
+    _columns={
+            "Address2":fields.text("Address2",size=128),
+            }
+partner_multi_address()
